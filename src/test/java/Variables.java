09d3be36769381b3eab8d00279b19de37facec22
And-Ray <redandray@yandex.ru>
2021-09-16 16:52:32 +0300

    variables

---
+++ src/test/java/Variables.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+public class Variables {
+    public int a = 2;
+    public int b = 3;
+
+    @Test
+    public void calculate(){
+        int c = a + b;
+        System.out.println("result = " + c);
+    }
+
+}
