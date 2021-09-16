9eedc1620a3b4892e6af2cff9a541843da1cc57f
KseNav <ksuk4you.ru@gmail.com>
2021-09-16 20:22:50 +0300

    calculate 3 digits dif

--- src/test/java/Variables.java
+++ src/test/java/Variables.java
@@ -2,8 +2,8 @@ import org.testng.annotations.Test;
 
 public class Variables {
     public int a = 2;
-    public int b = 4;
-    public int d = 12;
+    public int b = 8;
+    public int d = 8;
 
     @Test
     public void calculate(){
