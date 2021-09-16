f280418d0ab5e5f12056ad9fa8ae505ca17c391c
ROMA XXXXX <pochtarom@mail.ru>
2021-09-16 19:09:57 +0300

    calculate 3 digits

--- src/test/java/Variables.java
+++ src/test/java/Variables.java
@@ -3,10 +3,11 @@ import org.testng.annotations.Test;
 public class Variables {
     public int a = 2;
     public int b = 3;
+    public int d = 10;
 
     @Test
     public void calculate(){
-        int c = a + b;
+        int c = a + d+b;
         System.out.println("result = " + c);
     }
 
