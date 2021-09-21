e933c28cbd61cf85fa95c72f10b2097fdd40d647
ROMA XXXXX <pochtarom@mail.ru>
2021-09-21 20:53:53 +0300

    3 цикла

--- src/test/java/Variables.java
+++ src/test/java/Variables.java
@@ -12,14 +12,63 @@ public class Variables {
 
     //todo написать еще методы калькулятора
 
+    //@Test
+//    public void calculate() {
+//        System.out.println("Сложение целых чисел = " + (a + b));
+//        System.out.println("Сложение чисел long = " + (c + d));
+//        System.out.println("Сложение чисел double = " + (e + f));
+//        System.out.println("Сложение числа и строки = " + (c + str1));
+//        System.out.println("Сложение строк с пробелом = " + (String.join(" ", str1, str2)));
+//
+//    }
+
+    //@Test
+//    public void cycleIf() {
+//        if (a + b > 5) {
+//            System.out.println("сумма а и б больше 5");
+//            if (a<3){
+//                System.out.println("а меньше 3");
+//            }
+//        } else {
+//            System.out.println("сумма а и б меньше 5");
+//        }
+//    }
     @Test
-    public void calculate() {
-        System.out.println("Сложение целых чисел = " + (a + b));
-        System.out.println("Сложение чисел long = " + (c + d));
-        System.out.println("Сложение чисел double = " + (e + f));
-        System.out.println("Сложение числа и строки = " + (c + str1));
-        System.out.println("Сложение строк с пробелом = " + (String.join(" ",str1, str2)));
+//    public void test1() {
+//        int sum = 0;
+//        for (int i = 1; i < 21; i++) {
+//            int j = i * 2;
+//            System.out.println("Умножение " + (j));
+//            sum = sum + j;
+//        }
+//        System.out.println("Сумма " + sum);
+//    }
+//    public void test2() {
+//        for (int i = 35; i > 3; i--) {
+//            if (i % 3 == 0) {
+//                System.out.println("Результат деления без остатка " + (i/3));
+//            } else {
+//                System.out.println("Мимо");
+//            }
+//        }
+//    }
 
+    public void test3() {
+        int sum = 0;
+        for (int i = 1; i < 51; i++) {
+            sum = sum + i;
+        }
+        System.out.println("Сумма чисел от 1 до 50 " + sum);
+        System.out.println("Начат отсчет:");
+        int n = 1;
+do {
+    sum = sum - 500;
+    if (sum < 200){
+        break;
     }
-
+    System.out.println((sum));
+    n++;
+}
+while (sum>199);
+    }
 }
